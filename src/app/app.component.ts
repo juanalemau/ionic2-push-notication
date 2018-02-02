b93d43c67eae63eb7d721c7cd3faa2ec75dd7555
Juan Alejandro Mauricio Aguirre <jmaurica@everis.com>
2018-02-02 14:49:48 -0500

    [update] demo push notification

---
+++ src/app/app.component.ts
@@ -0,0 +1,68 @@
+import { Component } from '@angular/core';
+import { Platform, AlertController } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { Push, PushObject, PushOptions} from '@ionic-native/push';
+
+import { HomePage } from '../pages/home/home';
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  rootPage:any = HomePage;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public push: Push, public alertCtrl:AlertController) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      this.pushsetup();
+      splashScreen.hide();
+    });
+  }
+
+  pushsetup() {
+    const options: PushOptions = {
+     android: {
+         senderID: '253835947852'
+     },
+     ios: {
+         alert: 'true',
+         badge: true,
+         sound: 'false'
+     },
+     windows: {}
+    };
+
+    const pushObject: PushObject = this.push.init(options);
+
+    pushObject.on('notification').subscribe((notification: any) => {
+      if (notification.additionalData.foreground) {
+        let youralert = this.alertCtrl.create({
+          title: 'New Push notification',
+          message: notification.message,
+          buttons: [{
+              text: 'Ignore',
+              role: 'cancel'
+            }, {
+              text: 'View',
+              handler: () => {
+                //TODO: Your logic here
+                 console.log('ingrese mensaje');
+                /*this.nav.push(DetailsPage, { message: data.message });*/
+              }
+            }]
+        });
+        youralert.present();
+      }
+    });
+
+    pushObject.on('registration').subscribe((registration: any) => {
+     //do whatever you want with the registration ID
+    });
+
+    pushObject.on('error').subscribe(error => alert('Error with Push plugin' + error));
+    
+  }
+}  
+
